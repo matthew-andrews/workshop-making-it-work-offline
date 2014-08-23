bde61e4b12f5161c8f4514eeccd9a9a8fe5c77ac
Matthew Andrews <matt@mattandre.ws>
2014-08-23 13:22:33 +0100

    Add source files into 03/04

---
+++ 03-offline-todo/04-creating-object-stores/application.js
@@ -0,0 +1,28 @@
+(function() {
+
+  // 'global' variable to store reference to the database
+  var db;
+
+  databaseOpen()
+    .then(function() {
+      alert("The database has been opened");
+    });
+
+  function databaseOpen(callback) {
+    return new Promise(function(resolve, reject) {
+      var version = 1;
+      var request = indexedDB.open('todos', version);
+      request.onupgradeneeded = function(e) {
+        db = e.target.result;
+        e.target.transaction.onerror = reject;
+        db.createObjectStore('todo', { keyPath: '_id' });
+      };
+      request.onsuccess = function(e) {
+        db = e.target.result;
+        resolve();
+      };
+      request.onerror = reject;
+    });
+  }
+
+}());
