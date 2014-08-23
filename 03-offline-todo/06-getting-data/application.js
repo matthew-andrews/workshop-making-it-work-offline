e9f2ff14a90a6148c45ca7c91885e54b2198540d
Matthew Andrews <matt@mattandre.ws>
2014-08-23 19:13:34 +0100

    Add source code to 03/06

---
+++ 03-offline-todo/06-getting-data/application.js
@@ -0,0 +1,78 @@
+(function() {
+  var db, input;
+
+  databaseOpen()
+    .then(function() {
+      input = document.getElementsByTagName('input')[0];
+      document.body.addEventListener('submit', onSubmit);
+    })
+    .then(databaseTodosGet)
+    .then(function(todos) {
+      console.log(todos);
+    });
+
+  function onSubmit(e) {
+    e.preventDefault();
+    var todo = { text: input.value, _id: String(Date.now()) };
+    databasesTodosPut(todo)
+      .then(function() {
+        input.value = '';
+      });
+  }
+
+  function databaseOpen() {
+    return new Promise(function(resolve, reject) {
+      var version = 1;
+      var request = indexedDB.open('todos', version);
+      request.onupgradeneeded = function(e) {
+        db = e.target.result;
+        e.target.transaction.onerror = reject;
+        db.createObjectStore('todo', { keyPath: '_id' });
+      };
+      request.onsuccess = function(e) {
+        db = e.target.result;
+        resolve();
+      };
+      request.onerror = reject;
+    });
+  }
+
+  function databasesTodosPut(todo) {
+    return new Promise(function(resolve, reject) {
+      var transaction = db.transaction(['todo'], 'readwrite');
+      var store = transaction.objectStore('todo');
+      var request = store.put(todo);
+      request.onsuccess = resolve;
+      request.onerror = reject;
+    });
+  }
+
+  function databaseTodosGet() {
+    return new Promise(function(resolve, reject) {
+      var transaction = db.transaction(['todo'], 'readwrite');
+      var store = transaction.objectStore('todo');
+
+      // Get everything in the store
+      var keyRange = IDBKeyRange.lowerBound(0);
+      var cursorRequest = store.openCursor(keyRange);
+
+      // This fires once per row in the store, so for simplicity collect the data
+      // in an array (data) and send it pass it in the resolve call in one go
+      var data = [];
+      cursorRequest.onsuccess = function(e) {
+        var result = e.target.result;
+
+        // If there's data, add it to array
+        if (result) {
+          data.push(result.value);
+          result.continue();
+
+        // Reach the end of the data
+        } else {
+          resolve(data);
+        }
+      };
+    });
+  }
+
+}());
