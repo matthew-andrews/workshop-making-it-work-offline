92208e35216c21897931513f8d69b471a6802084
Matthew Andrews <matt@mattandre.ws>
2014-08-24 00:34:33 +0100

    Implement the code samples for rendering

---
+++ 03-offline-todo/08-rendering-todos/application.js
@@ -0,0 +1,92 @@
+(function() {
+  var db, input, ul;
+
+  databaseOpen()
+    .then(function() {
+      input = document.getElementsByTagName('input')[0];
+      ul = document.getElementsByTagName('ul')[0];
+      document.body.addEventListener('submit', onSubmit);
+    })
+    .then(refreshView);
+
+  function onSubmit(e) {
+    e.preventDefault();
+    var todo = { text: input.value, _id: String(Date.now()) };
+    databasesTodosPut(todo)
+      .then(function() {
+        input.value = '';
+      });
+  }
+
+  function refreshView() {
+    return databaseTodosGet().then(renderAllTodos);
+  }
+
+  function renderAllTodos(todos) {
+    var html = '';
+    todos.forEach(function(todo) {
+      html += todoToHtml(todo);
+    });
+    ul.innerHTML = html;
+  }
+
+  function todoToHtml(todo) {
+    return '<li>'+todo.text+'</li>';
+  }
+
+  function databaseOpen() {
+    return new Promise(function(resolve, reject) {
+      var version = 1;
+      var request = indexedDB.open('todos', version);
+      request.onupgradeneeded = function(e) {
+        db = e.target.result;
+        e.target.transaction.onerror = reject;
+        db.createObjectStore('todo', { keyPath: '_id' });
+      };
+      request.onsuccess = function(e) {
+        db = e.target.result;
+        resolve();
+      };
+      request.onerror = reject;
+    });
+  }
+
+  function databasesTodosPut(todo) {
+    return new Promise(function(resolve, reject) {
+      var transaction = db.transaction(['todo'], 'readwrite');
+      var store = transaction.objectStore('todo');
+      var request = store.put(todo);
+      request.onsuccess = resolve;
+      request.onerror = reject;
+    });
+  }
+
+  function databaseTodosGet() {
+    return new Promise(function(resolve, reject) {
+      var transaction = db.transaction(['todo'], 'readwrite');
+      var store = transaction.objectStore('todo');
+
+      // Get everything in the store
+      var keyRange = IDBKeyRange.lowerBound(0);
+      var cursorRequest = store.openCursor(keyRange);
+
+      // This fires once per row in the store, so for simplicity collect the data
+      // in an array (data) and send it pass it in the resolve call in one go
+      var data = [];
+      cursorRequest.onsuccess = function(e) {
+        var result = e.target.result;
+
+        // If there's data, add it to array
+        if (result) {
+          data.push(result.value);
+          result.continue();
+
+        // Reach the end of the data
+        } else {
+          resolve(data);
+        }
+      };
+    });
+  }
+
+}());
