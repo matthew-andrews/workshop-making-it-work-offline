a2d57006c70e78a2e71d62d0602f4866f3c96e77
Matthew Andrews <matt@mattandre.ws>
2014-09-06 14:36:26 +0100

    Add iframe hack code

---
+++ 05-offline-news/03-hacking-appcache/index.js
@@ -0,0 +1,84 @@
+var api = 'https://offline-news-api.herokuapp.com/stories';
+var port = 8080;
+var express = require('express');
+var path = require('path');
+var request = require('superagent');
+var templates = require('./public/templates');
+
+var app = express();
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/offline.appcache', function(req, res) {
+  res.set('Content-Type', 'text/cache-manifest');
+  res.send('CACHE MANIFEST'
+    + '\n./application.js'
+    + '\n./indexeddb.shim.min.js'
+    + '\n./promise.js'
+    + '\n./styles.css'
+    + '\n./superagent.js'
+    + '\n./templates.js'
+    + '\n'
+    + '\nFALLBACK:'
+    + '\n/ /'
+    + '\n'
+    + '\nNETWORK:'
+    + '\n*');
+});
+
+app.get('/article/:guid', function(req, res) {
+  request.get(api+'/'+req.params.guid)
+    .end(function(err, data) {
+      if (err || !data.ok) {
+        res.status(404);
+        res.send(layoutShell({
+          main: templates.article({
+            title: 'Story cannot be found',
+            body: '<p>Please try another</p>'
+          })
+        }));
+      } else {
+        res.send(layoutShell({
+          main: templates.article(data.body)
+        }));
+      }
+    });
+});
+
+app.get('/', function(req, res) {
+  request.get(api)
+    .end(function(err, data) {
+      if (err) {
+        res.status(404).end();
+      } else {
+        res.send(layoutShell({
+          main: templates.list(data.body)
+        }));
+      }
+    });
+});
+
+function layoutShell(data) {
+  data = {
+    title: data && data.title || 'FT Tech News',
+    main: data && data.main || ''
+  };
+  return '<!DOCTYPE html>'
+    + '\n<html>'
+    + '\n  <head>'
+    + '\n    <title>'+data.title+'</title>'
+    + '\n    <link rel="stylesheet" href="/styles.css" type="text/css" media="all" />'
+    + '\n  </head>'
+    + '\n  <body>'
+    + '\n    <main>'+data.main+'</main>'
+    + '\n    <script src="/indexeddb.shim.min.js"></script>'
+    + '\n    <script src="/superagent.js"></script>'
+    + '\n    <script src="/promise.js"></script>'
+    + '\n    <script src="/templates.js"></script>'
+    + '\n    <script src="/application.js"></script>'
+    + '\n    <iframe src="/iframe.html" style="width:0px; height:0px; visibility:hidden; position:absolute; border:none"></iframe>'
+    + '\n  </body>'
+    + '\n</html>';
+}
+
+app.listen(port);
+console.log('listening on port', port);
---
+++ 05-offline-news/03-hacking-appcache/public/application.js
@@ -0,0 +1,170 @@
+(function() {
+  var api = 'http' + (location.hostname === 'localhost' ? '://localhost:3000' : 's://offline-news-api.herokuapp.com') + '/stories';
+  var synchronizeInProgress;
+  var db, main;
+
+  databaseOpen()
+    .then(function() {
+      main = document.querySelector('main');
+      document.body.addEventListener('click', onClick);
+      window.addEventListener('popstate', refreshView);
+
+      // Only refresh the view if the view is empty
+      if (main.innerHTML === '') return refreshView();
+    })
+    .then(synchronize);
+
+  function onClick(e) {
+    if (e.target.classList.contains('js-link')) {
+      e.preventDefault();
+      history.pushState({}, '', e.target.getAttribute('href'));
+      refreshView();
+    }
+  }
+
+  function refreshView() {
+    var guidMatches = location.pathname.match(/^\/article\/([0-9]+)/);
+    if (!guidMatches) {
+      renderAllStories();
+      return databaseStoriesGet().then(renderAllStories);
+    }
+    renderOneStory();
+    return databaseStoriesGetById(guidMatches[1]).then(renderOneStory);
+  }
+
+  function renderAllStories(stories) {
+    main.innerHTML = templates.list(stories);
+  }
+
+  function renderOneStory(story) {
+    if (!story) story = { title: 'Story cannot be found', body: '<p>Please try another</p>' };
+    main.innerHTML = templates.article(story);
+  }
+
+  function synchronize() {
+    if (synchronizeInProgress) return synchronizeInProgress;
+    synchronizeInProgress = Promise.all([serverStoriesGet(), databaseStoriesGet()])
+      .then(function(results) {
+        var promises = [];
+        var remoteStories = results[0];
+        var localStories = results[1];
+
+        // Add new stories downloaded from server to the database
+        promises = promises.concat(remoteStories.map(function(story) {
+          if (!arrayContainsStory(localStories, story)) {
+            return databaseStoriesPut(story);
+          }
+        }));
+
+        // Delete stories that are no longer on the server from the database
+        promises = promises.concat(localStories.map(function(story) {
+          if (!arrayContainsStory(remoteStories, story)) {
+            return databaseStoriesDelete(story);
+          }
+        }));
+
+        return promises;
+      })
+
+      // Only refresh the view if it's listing page
+      .then(function(results) {
+        if (location.pathname === '/') {
+          return refreshView();
+        }
+      })
+      .then(function() {
+        synchronizeInProgress = undefined;
+      });
+  }
+
+  function arrayContainsStory(array, story) {
+    return array.some(function(arrayStory) {
+      return arrayStory.guid === story.guid;
+    });
+  }
+
+  function databaseOpen() {
+    return new Promise(function(resolve, reject) {
+      var version = 1;
+      var request = indexedDB.open('news-server-rendered', version);
+      request.onupgradeneeded = function(e) {
+        db = e.target.result;
+        e.target.transaction.onerror = reject;
+        db.createObjectStore('stories', { keyPath: 'guid' });
+      };
+      request.onsuccess = function(e) {
+        db = e.target.result;
+        resolve();
+      };
+      request.onerror = reject;
+    });
+  }
+
+  function databaseStoriesPut(story) {
+    return new Promise(function(resolve, reject) {
+      var transaction = db.transaction(['stories'], 'readwrite');
+      var store = transaction.objectStore('stories');
+      var request = store.put(story);
+      request.onsuccess = resolve;
+      request.onerror = reject;
+    });
+  }
+
+  function databaseStoriesGet() {
+    return new Promise(function(resolve, reject) {
+      var transaction = db.transaction(['stories'], 'readwrite');
+      var store = transaction.objectStore('stories');
+
+      var keyRange = IDBKeyRange.lowerBound(0);
+
+      // Using reverse direction because the index being sorted on
+      // ends with a numerical incrementing ID so to get newest news
+      // first you need to sort by largest first.
+      var cursorRequest = store.openCursor(keyRange, 'prev');
+
+      var data = [];
+      cursorRequest.onsuccess = function(e) {
+        var result = e.target.result;
+        if (result) {
+          data.push(result.value);
+          result.continue();
+        } else {
+          resolve(data);
+        }
+      };
+    });
+  }
+
+  function databaseStoriesGetById(guid) {
+    return new Promise(function(resolve, reject) {
+      var transaction = db.transaction(['stories'], 'readwrite');
+      var store = transaction.objectStore('stories');
+      var request = store.get(guid);
+      request.onsuccess = function(e) {
+        var result = e.target.result;
+        resolve(result);
+      };
+      request.onerror = reject;
+    });
+  }
+
+  function databaseStoriesDelete(story) {
+    return new Promise(function(resolve, reject) {
+      var transaction = db.transaction(['stories'], 'readwrite');
+      var store = transaction.objectStore('stories');
+      var request = store.delete(story.guid);
+      request.onsuccess = resolve;
+      request.onerror = reject;
+    });
+  }
+
+  function serverStoriesGet(guid) {
+    return new Promise(function(resolve, reject) {
+      superagent.get(api+'/' + (guid ? guid : ''))
+        .end(function(err, res) {
+          if (!err && res.ok) resolve(res.body);
+          else reject(res);
+        });
+    });
+  }
+})();
