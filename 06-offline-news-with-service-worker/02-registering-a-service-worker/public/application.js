434b66eb1d0301b1292987dcfec6d24e17674b3d
Matthew Andrews <matt@mattandre.ws>
2014-10-25 22:40:37 +0100

    Add solution files to step 2

---
+++ 06-offline-news-with-service-worker/02-registering-a-service-worker/public/application.js
@@ -0,0 +1,5 @@
+(function() {
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker.register('/service-worker.js');
+	}
+}());
---
+++ 06-offline-news-with-service-worker/02-registering-a-service-worker/public/service-worker.js
@@ -0,0 +1,6 @@
+console.log("I am a Service Worker and I'm OK");
+
+this.oninstall = function(event) {
+	// TODO: Something interesting
+	console.log("The Service Worker has been installed");
+};
