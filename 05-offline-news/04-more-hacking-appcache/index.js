0b76f8ff50886303944e3ce5905f1e7749847e08
Matthew Andrews <matt@mattandre.ws>
2014-11-08 16:22:42 +0000

    superagent -> iso fetch

--- 05-offline-news/04-more-hacking-appcache/index.js
+++ 05-offline-news/04-more-hacking-appcache/index.js
@@ -1,10 +1,12 @@
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
 var api = 'https://offline-news-api.herokuapp.com/stories';
 var port = 8080;
 
 var cookieParser = require('cookie-parser');
 var express = require('express');
 var path = require('path');
-var request = require('superagent');
 var templates = require('./public/templates');
 
 var app = express();
@@ -44,34 +46,36 @@ function offlineMiddleware(req, res, next) {
 }
 
 app.get('/article/:guid', function(req, res) {
-	request.get(api+'/'+req.params.guid)
-		.end(function(err, data) {
-			if (err || !data.ok) {
-				res.status(404);
-				res.send(layoutShell({
-					main: templates.article({
-						title: 'Story cannot be found',
-						body: '<p>Please try another</p>'
-					})
-				}));
-			} else {
-				res.send(layoutShell({
-					main: templates.article(data.body)
-				}));
-			}
+	fetch(api+'/'+req.params.guid)
+		.then(function(response) {
+			return response.json();
+		})
+		.then(function(data) {
+			res.send(layoutShell({
+				main: templates.article(data)
+			}));
+		}, function(err) {
+			res.status(404);
+			res.send(layoutShell({
+				main: templates.article({
+					title: 'Story cannot be found',
+					body: '<p>Please try another</p>'
+				})
+			}));
 		});
 });
 
 app.get('/', function(req, res) {
-	request.get(api)
-		.end(function(err, data) {
-			if (err) {
-				res.status(404).end();
-			} else {
-				res.send(layoutShell({
-					main: templates.list(data.body)
-				}));
-			}
+	fetch(api)
+		.then(function(response) {
+			return response.json();
+		})
+		.then(function(data) {
+			res.send(layoutShell({
+				main: templates.list(data)
+			}));
+		}, function(err) {
+			res.status(404).end();
 		});
 });
 
@@ -82,21 +86,21 @@ function layoutShell(data) {
 	};
 	return '<!DOCTYPE html>'
 		+ '\n<html>'
-		+ '\n	<head>'
-		+ '\n		<title>'+data.title+'</title>'
-		+ '\n		<link rel="stylesheet" href="/styles.css" type="text/css" media="all" />'
-		+ '\n	</head>'
-		+ '\n	<body>'
-		+ '\n		<main>'+data.main+'</main>'
-		+ '\n		<script src="/indexeddb.shim.min.js"></script>'
-		+ '\n		<script src="/fetch.js"></script>'
-		+ '\n		<script src="/promise.js"></script>'
-		+ '\n		<script src="/templates.js"></script>'
-		+ '\n		<script src="/appcache.js"></script>'
-		+ '\n		<script src="/application.js"></script>'
-		+ '\n	</body>'
+		+ '\n  <head>'
+		+ '\n    <title>'+data.title+'</title>'
+		+ '\n    <link rel="stylesheet" href="/styles.css" type="text/css" media="all" />'
+		+ '\n  </head>'
+		+ '\n  <body>'
+		+ '\n    <main>'+data.main+'</main>'
+		+ '\n    <script src="/indexeddb.shim.min.js"></script>'
+		+ '\n    <script src="/fetch.js"></script>'
+		+ '\n    <script src="/promise.js"></script>'
+		+ '\n    <script src="/templates.js"></script>'
+		+ '\n    <script src="/appcache.js"></script>'
+		+ '\n    <script src="/application.js"></script>'
+		+ '\n  </body>'
 		+ '\n</html>';
 }
 
 app.listen(port);
-console.log('listening on '+port);
+console.log('listening on port', port);
